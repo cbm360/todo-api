ca5b3338607e7510b3a636e227ca9db4cbb55900
Camron Moore <camronbmoore@gmail.com>
2016-06-10 16:07:19 -0600

    Added post route for todo

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Meet mom for lunch',
-    completed: false
-}, {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-}, {
-    id: 3,
-    description: 'Feed the dog',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
 
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
 });
 
+// GET /todos
 app.get('/todos', function (req, res) {
     res.json(todos);
 });
 
+// GET /todos/:id
 app.get('/todo/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo;
@@ -39,6 +33,19 @@ app.get('/todo/:id', function (req, res) {
     } else {
         res.status(404).send('No ID matches the request');
     }
+});
+
+// POST /todos
+app.post('/todos', function (req, res) {
+    var body = req.body;
+
+    //add id field
+    body.id = todoNextId ++;
+
+    //push body into array
+    todos.push(body);
+
+    res.json(body);
 });
 
 app.listen(PORT, function () {
