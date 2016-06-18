47e2c90cd4d6b53b8f7834976690657734bea96f
CAMRON MOORE <camronmoore@CAMRONs-MBP.lan>
2016-06-18 14:48:01 -0600

    Refactored git delete todos to sequalize

--- server.js
+++ server.js
@@ -67,19 +67,23 @@ app.post('/todos', function(req, res) {
 // DELETE /todos/:id
 app.delete('/todo/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {
-        id: todoId
+
+    db.todo.destroy({
+        where: {
+            id: todoId
+        }
+    }).then(function (rowsDeleted) {
+        if (rowsDeleted === 0) {
+            res.status(404).json({
+                error: 'No todo with id'
+            });
+        } else {
+            res.status(204).send();
+        }
+
+    }, function(e) {
+        res.status(500).send();
     });
-
-    if (!matchedTodo) {
-        res.status(404).json({
-            "error": "No todo found with that id."
-        });
-    } else {
-        todos = _.without(todos, matchedTodo);
-        res.json(matchedTodo);
-    }
-
 });
 
 // PUT /todo/:id
